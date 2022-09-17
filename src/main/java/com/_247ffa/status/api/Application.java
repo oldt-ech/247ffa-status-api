bc6b756df32addab6aa373bda64e777751456d06
oldt-ech <oldtech@247ffa.com>
2022-09-16 21:25:30 -0400

    one more try...

--- src/main/java/com/_247ffa/status/api/Application.java
+++ src/main/java/com/_247ffa/status/api/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
-	public static int SECONDS_IN_HOUR = 3600;
+	public static int MILLISECONDS_IN_HOUR = 3600000;
 	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
